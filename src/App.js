54fb602fe5dbd0549f9c193d33c13980919df34b
sho-itsuka <sho.itsuka122@gmail.com>
2023-02-07 23:55:10 +0900

    Googleログインを実装

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
 
 import Home       from './components/Home';
 import CreatePost from './components/CreatePost';
@@ -8,13 +9,15 @@ import Logout     from './components/Logout';
 import Navbar     from './components/Navbar';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path='/'           element={<Home />}></Route>
         <Route path='/createpost' element={<CreatePost />}></Route>
-        <Route path='/login'     element={<Login />}></Route>
+        <Route path='/login'      element={<Login setIsAuth={setIsAuth}/>}></Route>
         <Route path='/logout'     element={<Logout />}></Route>
       </Routes>
     </Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,21 @@
+import { signInWithPopup } from 'firebase/auth'
+import { auth, provider } from '../firebase'
+
 import React from 'react'
 
-const Login = () => {
+const Login = ({ setIsAuth }) => {
+  const loginInWithGoogle = () => {
+    // Googleでログイン
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+    })
+  }
   return (
-    <div>Login</div>
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログイン</button>
+    </div>
   )
 }
 
