b5f579090286309c20d7cb4dd687785576ea51ec
sho-itsuka <sho.itsuka122@gmail.com>
2023-02-10 01:18:00 +0900

    投稿ページのスタイリング

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -4,18 +4,20 @@ import "./CreatePost.css";
 const CreatePost = () => {
   return (
     <div className='createPostPage'>
-      <h1>記事を投降する</h1>
-      <div className="inputPost">
-        <div>タイトル</div>
-        <input type="text" placeholder='タイトルを入力' />
+      <div className='postContainer'>
+        <h1>記事を投降する</h1>
+        <div className="inputPost">
+          <div>タイトル</div>
+          <input type="text" placeholder='タイトルを入力' />
+        </div>
+        <div className="inputPost">
+          <div>投稿</div>
+          <textarea placeholder='投降用の内容を記入' />
+        </div>
+        <button className='postButton'>投稿する</button>
       </div>
-      <div className="inputPost">
-      <div>投稿</div>
-        <textarea placeholder='投降用の内容を記入' />
-      </div>
-      <button className='postButton'>投稿する</button>
     </div>
-  )
-}
+  );
+};
 
 export default CreatePost
