c5f79336c6e6864015f84b014b2e8df51bec3f79
sho-itsuka <sho.itsuka122@gmail.com>
2023-02-10 00:54:01 +0900

    投稿ページの作成

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import "./CreatePost.css";
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div className='createPostPage'>
+      <h1>記事を投降する</h1>
+      <div className="inputPost">
+        <div>タイトル</div>
+        <input type="text" placeholder='タイトルを入力' />
+      </div>
+      <div className="inputPost">
+      <div>投稿</div>
+        <textarea placeholder='投降用の内容を記入' />
+      </div>
+      <button className='postButton'>投稿する</button>
+    </div>
   )
 }
 
