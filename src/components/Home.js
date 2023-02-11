b4a5941f66b4f1da6872fa3a6c3489578443ba46
sho-itsuka <sho.itsuka122@gmail.com>
2023-02-11 13:19:04 +0900

    Homeコンポーネントのスタイリング

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,24 @@
 import React from 'react'
+import "./Home.css"
 
 const Home = () => {
   return (
-    <div className='hopePage'>
+    <div className='homePage'>
       <div className="postContents">
         <div className="postHeader">
           <h1>タイトル</h1>
         </div>
-      </div>
-      <div className="postTextContainer">
-        <h3>自己紹介</h3>
-        2023年7月からWebエンジニアになりました。<br />
-        今はReactを学習中です。<br />
-        これからフロントエンジニアとしてスキルを高めて頑張っていきたいと思っています。<br />
-        よろしくお願いします。
-      </div>
-      <div className="nameAndDeleteButton">
-        <h3>@shota</h3>
-        <button>削除</button>
+        <div className="postTextContainer">
+          <h3>自己紹介</h3>
+          2023年7月からWebエンジニアになりました。<br />
+          今はReactを学習中です。<br />
+          これからフロントエンジニアとしてスキルを高めて頑張っていきたいと思っています。<br />
+          よろしくお願いします。
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@shota</h3>
+          <button>削除</button>
+        </div>
       </div>
     </div>
   )
