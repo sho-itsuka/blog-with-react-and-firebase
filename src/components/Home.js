a5f23e25f7a0f6847d7af451cca734928989d3e4
sho-itsuka <sho.itsuka122@gmail.com>
2023-02-11 12:45:50 +0900

    Homeコンポーネントの作成

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='hopePage'>
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+      </div>
+      <div className="postTextContainer">
+        <h3>自己紹介</h3>
+        2023年7月からWebエンジニアになりました。<br />
+        今はReactを学習中です。<br />
+        これからフロントエンジニアとしてスキルを高めて頑張っていきたいと思っています。<br />
+        よろしくお願いします。
+      </div>
+      <div className="nameAndDeleteButton">
+        <h3>@shota</h3>
+        <button>削除</button>
+      </div>
+    </div>
   )
 };
 
